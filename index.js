752b7b42f37f0296c685b837d713a3bf24a9b972
Nicholas Rynearson <nrynearson@gmail.com>
2021-06-08 19:54:55 -0400

    adds some additional styling, cleans up js, adds some pseudo-code js for interactivity

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // store the dropdown menu in a constant
 const dropDown = document.getElementById("dropdown");
 
-// write a function appendNames() to populate the dropdown menu with the names of the beaches; thge function should 1) create an option for each name and 2) find the value for the key "name" for each item in the response data and set it to the innerText of each option 
+// write a function appendNames() to populate the dropdown menu with the names of the beaches; thge function should 1) create an option for each name and 2) find the value for the key "Name" for each item in the response data and set it to the innerText of each option 
 
 const appendNames = (beachNames) => {
   beachNames.forEach (name => {
@@ -22,28 +22,28 @@ const getNames = async () => {
     for (let i = 0; i < response.data.length; i += 1) {
       beachNames.push(response.data[i].Name);
     }
-    // console.log(beachNames);
     appendNames(beachNames);
   } catch (error) {
     console.error(error.message)
   }
 }
 
+
+// call getNames() on page load
 getNames()
 
 
 
 
 
-// call getNames() on page load
 
 // write an async function getBeachInfo() to make an axios call to retrieve data for the beach selected and pass response to 
 
 // write a function displayBeachInfo() to display the main info on in the #info div of the display section
 
-// write a function displayDirections() to display directions & map in the #directions div of the display section
+// write a function displayDirections() to display directions & map in the #directions div of the display section and scroll to the top of that div
 
-// write a function displayAccessibility() to display detailed accessibility information  #accessibility div of the display section
+// write a function displayAccessibility() to display detailed accessibility information  #accessibility div of the display section and scroll to the top of that div
 
 
 // add event listener to the dropdown menu to call getBeachInfo() when the user selects a beach
@@ -52,4 +52,6 @@ getNames()
 
 // add event listener to the more info button in the main info display (in the #info div) to call displayAccessibility()
 
-// add event listener to the ""
+// add event listener to the "top" button to bring use back to the top of the page
+
+// add media query for mobile screen size breakpoint 
