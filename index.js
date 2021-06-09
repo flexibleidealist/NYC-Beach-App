ffa63e6c8fe19df8b2bb60d01d955e8b41ef22a0
Nicholas Rynearson <nrynearson@gmail.com>
2021-06-09 17:04:07 -0400

    adds function to get and display sata from API, adds styling to rendered data

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // store the dropdown menu in a constant
 const dropDown = document.getElementById("dropdown");
-
+const url = "https://cors-anywhere.herokuapp.com/https://www.nycgovparks.org/bigapps/DPR_Beaches_001.json"
 // write a function appendNames() to populate the dropdown menu with the names of the beaches; thge function should 1) create an option for each name and 2) find the value for the key "Name" for each item in the response data and set it to the innerText of each option 
 
 const appendNames = (beachNames) => {
@@ -15,8 +15,6 @@ const appendNames = (beachNames) => {
 
 const getNames = async () => {
   try { 
-    const url = "https://cors-anywhere.herokuapp.com/https://www.nycgovparks.org/bigapps/DPR_Beaches_001.json"
-  
     const response = await axios.get(url);
     const beachNames = [];
     for (let i = 0; i < response.data.length; i += 1) {
@@ -27,7 +25,15 @@ const getNames = async () => {
     console.error(error.message)
   }
 }
-
+// write an async function getBeachInfo() to make an axios call to retrieve data for the beach selected and pass response to / get the [i] of the beach object from beachNames?
+const getBeachInfo = async (beach) => {
+  try {
+    const response = await axios.get(url);
+    console.log(response.data[beach].Location); //[beach] needs to be defined by the # of the option selected by the user
+  } catch (error) {
+    console.error(error.message);
+  }
+}
 
 // call getNames() on page load
 getNames()
@@ -37,18 +43,42 @@ getNames()
 
 
 
-// write an async function getBeachInfo() to make an axios call to retrieve data for the beach selected and pass response to 
 
 // write a function displayBeachInfo() to display the main info on in the #info div of the display section
 
 // write a function displayDirections() to display directions & map in the #directions div of the display section and scroll to the top of that div
-
 // write a function displayAccessibility() to display detailed accessibility information  #accessibility div of the display section and scroll to the top of that div
 
 
 // add event listener to the dropdown menu to call getBeachInfo() when the user selects a beach
 
-// add event listener to the directions button in the main info display (in the #info div) to call displayDirections()
+dropDown.addEventListener("change", async () => {
+  document.getElementById("info").childNodes.forEach(node => {
+    node.remove();
+  });
+  try {
+    const response = await axios.get(url);
+    for (let i = 0; i < response.data.length; i += 1) {
+      if (response.data[i].Name === dropDown.value) {
+        const basicInfo = document.createElement("p");
+        basicInfo.innerHTML = `${response.data[i].Name}<br>Location: ${response.data[i].Location}<br>Bathrooms: ${response.data[i].Bathroom}<br>Accessible: ${response.data[i].Accessible}<br><br>${response.data[i].Description}`;
+        document.getElementById("info").append(basicInfo);
+        const directionsDiv = document.getElementById("directions");
+        directionsDiv.innerHTML = response.data[i].Directions;
+        const accessibilityDiv = getElementById("accessibility");
+        accessibilityDiv.innerHTML = response.data[i].Accessible_Notes;
+        document.getElementById("buttons-div").classList.remove("hidden");
+
+      }
+    }
+    
+  } catch (error) {
+    console.error(error.message);
+  }
+  
+})
+
+// add event listener to the directions button in the main info display (in the #info div) to toggle the display property of the #directions div to display on the page
 
 // add event listener to the more info button in the main info display (in the #info div) to call displayAccessibility()
 
